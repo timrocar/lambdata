d131f5f317442bda2cf07d1f9999f025d6f9c9be
timrocar <timrocar@gmail.com>
2020-08-31 20:59:01 -0400

    function correction

--- lambdata/functions.py
+++ lambdata/functions.py
@@ -8,18 +8,18 @@ import datetime
 
 def splitdate(df, date):
     #make col datetime
-    if (df['date'].dtype == object):
-        df['date'] = pd.to_datetime(df['date'])
-    elif (df['date'].dtype == np.float64 or df['date'].dtype == np.int64):
-        df['date'] = pd.to_datetime(df['date'], format='%Y%m%d')
+    if type(df[date]) == object:
+        df[date] = pd.to_datetime(df[date])
+    elif type(df[date]) == np.float64 or type(df[date]) == np.int64:
+        df[date] = pd.to_datetime(df[date], format='%Y%m%d')
     else:
-        print('col already in datetime... spliting..')
-        
+        df[date] = pd.to_datetime(df[date])
+         
     #then split into year, month, day
-    df['year'] = df['date'].dt.year
-    df['month'] = df['date'].dt.month
-    df['day'] = df['date'].dt.day
-
+    df['year'] = df[date].dt.year
+    df['month'] = df[date].dt.month
+    df['day'] = df[date].dt.day
+    
 def addlist(df, list):
     s = pd.Series(list, index=df.index)
     df['listcol'] = s
